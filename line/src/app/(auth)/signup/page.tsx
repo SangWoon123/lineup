aa0ea2fe8e3f7511f66f2aa52850b3c4eeac5154
SangWoon123 <alrax3574@naver.com>
2026-01-10 16:55:01 +0900

    chore: 프로젝트 구조 정리 (line 폴더 내용을 루트로 이동)

--- line/src/app/(auth)/signup/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SignUpForm } from '@/components/auth/sign-up-form';
-export default function SignUpPage() {
-    return (
-        <div className="w-full flex flex-col items-center justify-center">
-            <SignUpForm />
-        </div>
-    );
-}
