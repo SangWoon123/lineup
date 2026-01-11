fb4b652ef8c708f54537193942f68289f5985fa2
SangWoon123 <alrax3574@naver.com>
2026-01-11 22:31:50 +0900

    매장 등록시 수용인원 값 적용되도록 수정

--- line/src/app/actions.tsx
+++ line/src/app/actions.tsx
@@ -5,22 +5,19 @@ import { revalidatePath } from 'next/cache';
 export async function createStore(formData: FormData) {
     const name = formData.get('name') as string;
     const location = formData.get('location') as string;
-    const rawSeat = formData.get('seat') as string;
-    const capacity = 100;
+    const capacity = formData.get('seat') as string;
 
-    if (!name || !location || !rawSeat) {
+    if (!name || !location || !capacity) {
         return { success: false, message: '모든 필드를 입력해주세요.' };
     }
-
-    const seat = Number(rawSeat);
 
     try {
         await prisma.store.create({
             data: {
                 name: name,
                 location: location,
-                seat: seat,
-                capacity: capacity,
+                seat: 0,
+                capacity: Number(capacity),
             },
         });
 
